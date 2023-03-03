b89f4d61f9a35367d724b8a93ebbbf378235a84e
Shabnam27 <124490203+Shabnam27@users.noreply.github.com>
2023-03-03 10:23:37 +0500

    Add files via upload

---
+++ CARS/JS/script.js
@@ -0,0 +1,32 @@
+document.getElementById("main-action").onclick = function () {
+    document.getElementById("cars").scrollIntoView({behavior: "smooth"});
+}
+
+var buttons = document.getElementsByClassName("car-button");
+for (var i = 0; i < buttons.length; i++) {
+    buttons[i].onclick = function () {
+        document.getElementById("price").scrollIntoView({behavior: "smooth"});
+    }
+}
+
+document.getElementById("price-form").onclick = function () {
+    if (document.getElementById("name").value === "") {
+        alert("Заполните имя!");
+    } else if (document.getElementById("phone").value === "") {
+        alert("Заполните номер телефона!");
+    } else if (document.getElementById("car").value === "") {
+        alert("Выберите автомобиль!");
+    }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    let layer = document.querySelector('.price-image');
+document.addEventListener('mousemove', (event) => {
+        layer.style.transform = 'translate3d(' + ((event.clientX * 0.3) / 8) + 'px,' + ((event.clientY * 0.3) / 8) + 'px,0px)';
+});
+
+    const elem = document.querySelector(".main");
+document.addEventListener('scroll', () => {
+        elem.style.backgroundPositionX = '0' + (0.3 * window.pageYOffset) + 'px';
+})
+});
